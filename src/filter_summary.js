823592a3b388f7dd3390a7eccaf8b201b1a16a26
Sumit Shah <sumit8692@gmail.com>
2026-02-23 02:47:18 -0500

    Delete src directory

--- src/filter_summary.js
+++
@@ -1,9 +0,0 @@
-export default function getSummaryFromStockProfileData(value, stockSummaryData){
-      const summ = stockSummaryData.stocksProfileData[0];
-  
-      if (summ.hasOwnProperty(value)) {
-        const  summary = summ[value].summary;
-        return summary;
-      } 
-      return null;
-  }
--- src/math.js
+++
@@ -1,4 +0,0 @@
-export default function roundToDecimalPlaces(number, decimalPlaces) {
-    const factor = Math.pow(10, decimalPlaces);
-    return Math.round(number * factor) / factor;
-  }
