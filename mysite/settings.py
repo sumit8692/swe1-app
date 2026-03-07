c938ad25b7aaee81791509d16e302f21388fd04e
Sumit <sumit8692@gmail.com>
2026-03-07 16:21:11 -0500

    removed extra space

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,8 +27,7 @@ DEBUG = True
 
 # mysite/settings.py
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
