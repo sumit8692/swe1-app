57647623e85c219a11cf1c8bd5eb677e6524ad79
Ian <43066864+IanC13@users.noreply.github.com>
2026-03-09 18:39:02 -0400

    Add dummy test that always fails

--- polls/tests.py
+++ polls/tests.py
@@ -5,3 +5,6 @@ class BasicTest(TestCase):
     def test_basic(self):
         """Basic test to ensure testing works"""
         self.assertEqual(1, 1)
+
+    def test_dummy(self):
+        self.assertTrue(False)
