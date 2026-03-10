44338807b7ee3ed26430691fc4fb23a84e8d9bac
Sumit <sumit8692@gmail.com>
2026-03-09 21:00:13 -0400

    check main

--- polls/tests.py
+++ polls/tests.py
@@ -5,6 +5,3 @@ class BasicTest(TestCase):
     def test_basic(self):
         """Basic test to ensure testing works"""
         self.assertEqual(1, 1)
-
-    def test_dummy(self):
-        self.assertTrue(False)
